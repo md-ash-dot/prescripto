8a8ca443ae17a7d3066f23ca42da039cbed0df48
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2024-09-25 20:03:28 +0100

    Add styling to the group image on the left side header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
                 Book Appointment <br /> With Trusted Doctors
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-28' src={assets.group_profiles} alt="" />
                 <p>Simply brose through out extensive list of trusted doctors, <br /> schedule your appointment hassle-free.</p>
             </div>
             <a href="">
